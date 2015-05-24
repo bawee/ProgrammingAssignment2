31c240531630103d18abfa5c8e29cfc4f8bdd51f
bawee <bawee@hotmail.com>
2015-05-25 09:26:50 +1000

    2nd Draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,54 +1,62 @@
-#Thanks for marking this assignment
+#Thanks for marking this assignment!
 
-#The two functions below together calculates an inverse of a matrix.
+#The two functions below together calculates the inverse of a given matrix. 
+#The inverse is only calculated if a new matrix is given. 
+#If a new matrix is not given, the inverse matrix stored is retrieved from memory and not cacluated again.
 
-#
+#the first function, makeCacheMatrix, produces an "Object" containing the following four functions.
 
-
-# makeCacheMatrix produces a list of four functions:
-## i.   set -
-## ii.  get - 
-## iii. setInverse -
-## iv.  getInverse -
+## i.   set - stores a new matrix, and deletes the previously stored inverse, if any
+## ii.  get - reads in the matrix
+## iii. setInverse - sets the variable "inverse", where i is the inverse variable that is fed to it
+## iv.  getInverse - retrieves the inverse from the variable "inverse"
 
 makeCacheMatrix <- function(x) {
   
-  inverse <<- NULL
+  inverse <<- NULL #defines the variable "inverse", which stores the inverse matrix
   
-  set <- function(y) {
+  #i. set
+  set <- function(y) { 
     x <<- y
     inverse <<- NULL
   }
-
+  
+  #ii. get
   get <- function() x
   
+  #iii. setInverse
   setInverse <- function(i) inverse <<- i
   
+  #iv. getInverse
   getInverse <- function() inverse
   
+  
+  #returns a list of functions
   returnedList <- list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
-  returnedList
 }
 
-# This function checks if inverse is stored in "inverse". If not it calculates the inverse matrix using solve()
+# This 2nd function returns the inverse of a given matrix (not given directly but via makeCacheMatrix)
 
-cacheSolve <- function(leest) {
+cacheSolve <- function(mCMObject) { #takes in the list/"object", here called mCMObject from makeCacheMatrix
+
+#Part1: if inverse is already stored  
   
-  inverse <- leest$getInverse()
-  
-  if(!is.null(inverse)) {
+  inverse <- mCMObject$getInverse() #retrieve the inverse from mCMObject, if already stored
+
+  if(!is.null(inverse)) { #makes sure the inverse variable is not empty, and returns the previously stored inverse, instead of caclulating again
     message("getting cached data")
     return(inverse)
   }
-  
-  matrix <- leest$get()
 
-  inverse <- solve(matrix)
+#Part 2: if Inverse is not already stored  
   
-  leest$setInverse(inverse)
-  
-  inverse
+  matrix <- mCMObject$get() #gets the matrix that was given to makeCacheMatrix
 
+  inverse <- solve(matrix, ...) # if inverse is empty, calculate the inverse using solve()
+  
+  mcMObject$setInverse(inverse) # set the newly calculated inverse
+  
+  inverse #return the newly calculated inverse
 
 }
